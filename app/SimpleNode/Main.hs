9722ad2c916008052e761d95483c75ade50dd9a3
dariamelnikova <melnikova_daria@inbox.ru>
2018-03-19 19:10:04 +0300

    some expiriences with config file

--- app/SimpleNode/Main.hs
+++ app/SimpleNode/Main.hs
@@ -2,27 +2,41 @@ module Main where
 
 import              Control.Monad
 import              Control.Concurrent
+import              System.Environment (getArgs)
+
 import              Node.Node.Mining
 import              Node.Node.Types
+import              Service.Config
 import              Service.Timer
 import              Node.Lib
 import              Service.Metrics
 import              PoA
 import              CLI.CLI (control)
 
+
 main :: IO ()
 main = do
-    aExitChan <- newChan
-    aAnswerChan  <- newChan
-    metric $ increment "cl.node.count"
-    void $ startNode "./data/miningInitData.bin"
-        aExitChan aAnswerChan managerMining $ \ch aChan aMyNodeId -> do
-            -- периодически проверяем, в каком состоянии относительно сети
-            -- мы находимся
-            metronomeS 400000 (writeChan ch connectivityQuery)
-            metronomeS 1000000 (writeChan ch deleteOldestMsg)
-            metronomeS 10000000 (writeChan ch deleteDeadSouls)
-            metronomeS 3000000 $ writeChan ch deleteOldestVacantPositions
-            void $ forkIO $ servePoA  "1554" aMyNodeId ch aChan "1556"
-            void $ forkIO $ control "1555" ch
-    void $ readChan aExitChan
+    args <- getArgs
+    maybeConf <- findConfigFile args
+    case maybeConf of
+      Nothing     -> return ()
+      Just config -> do
+        aExitChan <- newChan
+        aAnswerChan  <- newChan
+        metric $ increment "cl.node.count"
+        void $ startNode "./data/miningInitData.bin"
+            aExitChan aAnswerChan managerMining $ \ch aChan aMyNodeId -> do
+                -- периодически проверяем, в каком состоянии относительно сети
+                -- мы находимся
+                metronomeS 400000 (writeChan ch connectivityQuery)
+                metronomeS 1000000 (writeChan ch deleteOldestMsg)
+                metronomeS 10000000 (writeChan ch deleteDeadSouls)
+                metronomeS 3000000 $ writeChan ch deleteOldestVacantPositions
+
+                Just poa_in  <- getVar config "SimpleNode" "poa_in"
+                Just poa_out <- getVar config "SimpleNode" "poa_out"
+                void $ forkIO $ servePoA poa_in  aMyNodeId ch aChan poa_out
+
+                Just rpc_port <- getVar config "RPC" "port"
+                void $ forkIO $ control rpc_port ch
+        void $ readChan aExitChan
---
+++ src/Service/Config.hs
@@ -0,0 +1,48 @@
+module Service.Config 
+     (
+       getVar
+     , defaultConfig
+     , findConfigFile
+     ) where
+
+import Data.Ini
+import Data.Text (pack, unpack)
+import System.Directory
+
+
+defaultConfig :: FilePath
+defaultConfig = "config.ini"
+
+getIni :: FilePath -> IO (Maybe Ini)
+getIni path = do
+        result <- readIniFile path
+        case result of
+          Left err  -> do
+               putStrLn err
+               return Nothing
+          Right ini -> return $ Just ini
+
+getVar :: FilePath -> String -> String -> IO (Maybe String)
+getVar path section key = do
+        maybeIni <- getIni path
+        case maybeIni of
+          Nothing  -> return Nothing
+          Just ini -> do
+               case lookupValue (pack section) (pack key) ini of
+                 Left err    -> do
+                      putStrLn err
+                      return Nothing
+                 Right value -> return $ Just $ unpack value 
+
+
+findConfigFile :: [String] -> IO (Maybe FilePath)
+findConfigFile args = if length args > 0
+                      then return $ Just $ head args
+                      else do
+                        ex <- doesFileExist defaultConfig
+                        if ex 
+                        then return $ Just defaultConfig
+                        else do
+                          putStrLn "Please, specify config file"
+                          return Nothing
+                         
