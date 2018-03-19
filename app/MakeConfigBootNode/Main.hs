0c13ab432aed90604c93ad26943e9f51032bc7b3
dariamelnikova <melnikova_daria@inbox.ru>
2018-03-19 19:10:04 +0300

    some expiriences with config file

--- app/MakeConfigBootNode/Main.hs
+++ app/MakeConfigBootNode/Main.hs
@@ -1,9 +1,20 @@
 {-# Language PackageImports #-}
 module Main where
 
+import System.Environment (getArgs)
+import Network.Socket     (PortNumber)
+import Service.Config
 import Node.Node.Config.Make
 import Node.Data.Data
 
 main :: IO ()
 main = do
-    makeFileConfig "./data/bootInitData.bin" [BootNode] 1666
+    args <- getArgs
+    maybeConf <- findConfigFile args
+    case maybeConf of
+      Nothing     -> return ()
+      Just config -> do 
+        maybePort <- getVar config "BootNode" "port"
+        case maybePort of
+          Nothing -> return () 
+          Just port -> makeFileConfig "./data/bootInitData.bin" [BootNode] ((read port)::PortNumber)
