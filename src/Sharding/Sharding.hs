ada10e8c8cedc9470422603b2f25a2a8ad4460d6
mixailflash <mixailflash@gmail.com>
2018-03-23 08:27:02 +0300

    change modules tree

--- src/Sharding/Sharding.hs
+++ src/Sharding/Sharding.hs
@@ -3,7 +3,10 @@
 {-# LANGUAGE ViewPatterns #-}
 module Sharding.Sharding where
 
-import              Sharding.Distance
+import              Sharding.Space.Distance
+import              Sharding.Space.Points
+import              Sharding.Types
+
 import              Control.Concurrent.Chan
 import              Data.List.Extra
 import              Control.Concurrent
@@ -14,62 +17,6 @@ import              Node.Data.Data
 import              Service.Timer
 import qualified    Data.Set            as S
 
-
-type BlockHash = (Word64, Word64, Word64, Word64, Word64, Word64, Word64, Word64)
-newtype MyPosition = MyPosition Point deriving (Eq, Ord, Show)
-newtype Position   = Position Point deriving (Eq, Ord, Show)
-
-data Block = Block BlockHash B.ByteString deriving (Ord, Eq, Show)
-
-data ShardingNode = ShardingNode {
-        nodeNeighbors           :: S.Set Neighbor
-    ,   shardingNodeId          :: MyNodeId
-    ,   nodePosition            :: MyPosition
-    ,   nodeIndex               :: S.Set BlockHash
-    ,   nodeDistance            :: Double -- think
-  }
-  deriving (Show, Eq, Ord)
-
-
-data Neighbor = Neighbor {
-        neighborDistance    :: Distance Point
-    ,   neighborPosition    :: Position
-    ,   neighborId          :: NodeId
-  }
-  deriving (Show, Eq, Ord)
-
-
-data ShardingNodeAction =
-    -- TODO think requestBlockIndex requestNeededBlocks, find position
-        InitAction
-    |   NewNodeInNetAction          NodeId Point
-    -- TODO create index for new node by NodeId
-    |   BlockIndexCreateAction      NodeId
-    |   BlockIndexAcceptAction      [BlockHash]
-    |   BlocksAcceptAction          [(BlockHash, Block)]
-    ---
-    |   CleanBlocksAction -- clean local blocks
-    --- ShiftAction => NewPosiotionResponse
-    |   ShiftAction
-    |   TheNodeHaveNewCoordinates   NodeId Position
-    ---- NeighborListRequest => NeighborListAcceptAction
-    |   NeighborListAcceptAction   [(NodeId, Position)]
-    |   TheNodeIsDead               NodeId
-
-
-data ShardingNodeRequestAndResponce =
-        IamAwakeRequst        NodeId Point -- broadcast for all network
-    ----
-    |   BlockIndexRequest     [NodeId]    -- for neighbors
-    |   BlockIndexResponse    NodeId [BlockHash]
-    |   BlockListRequest      [BlockHash]
-    |   BlockListResponse     NodeId [(BlockHash, Block)]
-    --- ShiftAction => NewPosiotionResponse
-    |   NewPosiotionResponse   MyPosition
-    ---
-    |   NeighborListRequest -- ask net level new neighbors
-
---
 
 makeEmptyShardingNode :: MyNodeId -> MyPosition -> S.Set BlockHash -> ShardingNode
 makeEmptyShardingNode aMyNodeId aMyPosition aMyBlockIndex = ShardingNode {
@@ -113,16 +60,6 @@ makeEmptyNeighbor aDistance aPosition aNodeId = Neighbor {
   }
 
 --------------------------------------------------------------------------------
--- | Find the support points.
-{-# INLINE findSupportPoints #-}
-findSupportPoints :: Point -> [Point]
-findSupportPoints (Point x1 x2) = [
-    Point (x1 + fourthOfMaxBound) x2,
-    Point (x1 - fourthOfMaxBound) x2,
-    Point x1 (x2 + fourthOfMaxBound),
-    Point x1 (x2 - fourthOfMaxBound)]
-
-
 findNodeDomain :: ShardingNode -> Distance Point
 findNodeDomain aShardingNode = if
     | length aNearestNeighbors < 4 -> maxBound
