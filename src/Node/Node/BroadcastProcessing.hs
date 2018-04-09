ad75127132b3a3aa70d2700024ca1ed9fb506dd4
dmitriy.pavlyuk <al.lastor@bk.ru>
2018-04-09 12:32:36 +0300

    fix bradcast

---
+++ src/Node/Node/BroadcastProcessing.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE
+    OverloadedStrings,
+    MultiWayIf,
+    LambdaCase,
+    MultiParamTypeClasses,
+    ViewPatterns,
+    StandaloneDeriving,
+    TypeSynonymInstances,
+    FlexibleContexts,
+    TypeFamilies,
+    FlexibleInstances
+#-}
+module Node.Node.BroadcastProcessing where
+
+--
+import qualified    Crypto.PubKey.ECC.ECDSA         as ECDSA
+
+import qualified    Data.ByteString                 as B
+import qualified    Data.Map                        as M
+import qualified    Data.Bimap                      as BI
+import              System.Clock
+import              Data.IORef
+import              Data.Serialize
+import              Lens.Micro
+import              Control.Concurrent
+import              Control.Monad.Extra
+import              Crypto.Error
+
+import              Service.Monad.Option
+import              Node.Crypto
+import              Node.Data.Data
+import              Node.Node.Types
+import              Node.Node.Base
+import              Node.Data.NodeTypes
+import              Node.Data.NetPackage
+import              Node.Data.NetMesseges
+import qualified    Sharding.Types.Node as T
+import              Sharding.Space.Point
+import              Node.Node.Processing
+import              Lens.Micro.GHC
+
+--
+class BroadcastProcessing aNodeData aPackage where
+    processingOfBroadcast :: aNodeData -> aPackage -> IO ()
+
+instance BroadcastProcessing (IORef ManagerNodeData) (BroadcastThingLvl NetLvl) where
+    processingOfBroadcast aMd aMsg = do
+        aData <- readIORef aMd
+        case aMsg of
+            INeedNeighbors (toNodeId -> aNodeId) aHostAddress aPortNumber ->
+                addRecordsToNodeListFile
+                    (aData^.myNodeId)
+                    (NodeInfoListNetLvl [(aNodeId, aHostAddress, aPortNumber)])
+
+instance BroadcastProcessing (IORef ManagerNodeData) (BroadcastThingLvl LogicLvl) where
+    processingOfBroadcast aMd aMsg = do
+        aData <- readIORef aMd
+        case aMsg of
+            BroadcastShard aShard -> do
+                whenJust (aData^.shardingChan) $ \aChan ->
+                    writeChan aChan $ T.NewShardInNetAction aShard
+
+            BroadcastPosition     aMyNodeId aNodePosition  -> do
+                updateFile aMyNodeId
+                    (NodeInfoListLogicLvl [(toNodeId aMyNodeId, aNodePosition)])
+                sendToShardingLvl aData $
+                    T.TheNodeHaveNewCoordinates (toNodeId aMyNodeId) aNodePosition
+
+instance BroadcastProcessing (IORef ManagerNodeData) (BroadcastThingLvl MiningLvl) where
+    processingOfBroadcast aMd aMsg = do
+        aData <- readIORef aMd
+        case aMsg of
+            BroadcastTransaction aTransaction _ ->
+                writeChan (aData^.transactions) aTransaction
+                -- metric $ add ("net.node." ++ show (toInteger $ aData^.myNodeId) ++ ".pending.amount") (1 :: Integer)
+            _ -> return ()
