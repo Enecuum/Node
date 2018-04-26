523ed2fdfc4cec68c17cf9af8f436c9dcc5fa0d8
dariamelnikova <melnikova_daria@inbox.ru>
2018-04-26 14:29:00 +0300

    TCP server update for testing

--- src/Service/Network/TCP/Server.hs
+++ src/Service/Network/TCP/Server.hs
@@ -5,22 +5,36 @@ import Network.Socket.ByteString
 
 import Control.Monad
 import Data.ByteString (ByteString)
-
+import qualified Control.Exception as E (bracket)
+import Control.Concurrent (forkFinally)
 
 -- | Run TCP server.
-runServer ::
-    PortNumber ->
-    (ByteString -> SockAddr -> Socket -> IO ())
-    -> IO ()
-runServer aPortNumber aPlainHandler = withSocketsDo $ do
-    aServerAddr:_ <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
-            Nothing (Just $ show aPortNumber)
-    aSocket <- socket (addrFamily aServerAddr) Stream defaultProtocol
-    setSocketOption aSocket ReuseAddr 1
-    bind aSocket (addrAddress aServerAddr)
-    listen aSocket 10
 
-    forever $ do
-        (conn, peer) <- accept aSocket
+runServer :: PortNumber
+          -> (ByteString -> SockAddr -> Socket -> IO())
+          -> IO ()
+runServer aPortNumber aPlainHandler = withSocketsDo $ do
+    addr <- resolve aPortNumber
+    E.bracket (open addr) close loop
+  where
+    resolve port = do
+        let hints = defaultHints {
+                addrFlags = [AI_PASSIVE]
+              , addrSocketType = Stream
+              }
+        addr:_ <- getAddrInfo (Just hints) Nothing (Just $ show port)
+        return addr
+    open addr = do
+        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+        setSocketOption sock ReuseAddr 1
+        bind sock (addrAddress addr)
+        listen sock 10
+        return sock
+    loop sock = forever $ do
+        (conn, peer) <- accept sock
+        putStrLn $ "Connection from " ++ show peer
+        void $ forkFinally (talk conn) (\_ -> close conn)
+    talk conn = do
         (aMsg, aHostAddress) <- recvFrom conn (1024*100)
         aPlainHandler aMsg aHostAddress conn
+        talk conn
