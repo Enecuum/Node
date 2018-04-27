5eccb1f5a5aaa7f02c3b394f0b20d0b068d4f4d1
mixailflash <mixailflash@gmail.com>
2018-04-27 14:04:27 +0300

    poa server

--- src/PoA.hs
+++ src/PoA.hs
@@ -7,7 +7,7 @@ module PoA (
 import              Data.Hex
 import qualified    Data.Serialize as S
 import              Control.Monad (forM_, void)
-import              Network.Socket.ByteString(sendAllTo)
+import              Network.Socket.ByteString(sendAllTo, sendAll)
 import              Service.Network.Base
 import              Service.Network.TCP.Client
 import              Service.Network.TCP.Server
@@ -17,9 +17,12 @@ import              Service.InfoMsg
 import              Service.Types
 import              Node.Extra
 
+import              Data.Aeson as A
+
 import              Node.Data.Data
 import              Node.Data.NodeTypes
 import              Node.Data.GlobalLoging
+import              PoA.Types
 {-
 writeLog :: String -> String -> IO ()
 writeLog aPath aString = do
@@ -41,6 +44,17 @@ servePoA ::
     -> Chan InfoMsg
     -> IO ()
 servePoA aRecivePort aSendPort aNodeId ch aRecvChan aInfoChan = runServer aRecivePort $
+    \aSocket -> do
+        sendAll aSocket $ A.encode RequestUUIDToPoAPoWMessage
+        aId <- newEmptyMVar
+        -- writeChan ch $ connecting to PoA, the PoA have id.
+        void $ race (aSender aId) (aReceiver aId)
+  where
+    aSender
+
+    aReceiver
+
+{-
     \aMsg aSockAddr _ -> do
         writeLog aInfoChan [ServePoATag] Info $ "PaA msg: " ++ (show $ hex $ aMsg)
         let aDecodeMsg = S.decode aMsg
@@ -62,8 +76,39 @@ servePoA aRecivePort aSendPort aNodeId ch aRecvChan aInfoChan = runServer aReciv
                 (-1 :: Integer)
             writeLog aInfoChan [ServePoATag] Info $  "sendTransaction to poa " ++ show aTransaction
             sendTransaction aHandle aTransaction
-
+-}
 -- | Send one transaction.
 sendTransaction :: ClientHandle -> Transaction -> IO ()
 sendTransaction aHandle aTransaction  = void $ sendAllTo
     (clientSocket aHandle) (S.encode aTransaction) (clientAddress aHandle)
+
+{-
+
+\aSocket -> forever $ do
+   (aMsg, addr) <- recvFrom aSocket (1024*100)
+
+socketActor
+    ::  ManagerMsg a
+    =>  HostAddress
+    ->  NodeId
+    ->  Chan a
+    ->  Chan MsgToSender
+    ->  WS.Connection
+    ->  IO ()
+socketActor _ aId aChan aInputChan aConnect = do
+    (void $ race sender receiver) `finally`
+        (writeChan aChan $ clientIsDisconnected aId aInputChan)
+  where
+    sender :: IO ()
+    sender = readChan aInputChan >>= \case
+        MsgToSender aMsg  -> do
+            WS.sendBinaryData aConnect aMsg >> sender
+        SenderExit aMsg   -> do
+            WS.sendBinaryData aConnect aMsg
+        SenderTerminate -> pure ()
+
+    receiver :: IO ()
+    receiver = forever $ do
+        aMsg <- WS.receiveDataMessage aConnect
+        writeChan aChan $ datagramMsg (WS.fromDataMessage aMsg) aId
+-}
