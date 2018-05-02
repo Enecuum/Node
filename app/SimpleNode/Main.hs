062bea569b7b7a2541b476f3c4cfa559c0435844
mixailflash <mixailflash@gmail.com>
2018-05-02 09:11:27 +0300

    fix warnigs

--- app/SimpleNode/Main.hs
+++ app/SimpleNode/Main.hs
@@ -40,13 +40,13 @@ main =  do
                             Left (_::SomeException) -> case simpleNodeBuildConfig conf of
                                  Nothing   -> error "Please, specify SimpleNodeConfig"
                                  Just snbc -> return $ poaInPort snbc
-
+{-
                     poa_out <- try (getEnv "poaOutPort") >>= \case
                             Right item              -> return $ read item
                             Left (_::SomeException) -> case simpleNodeBuildConfig conf of
                                  Nothing   -> error "Please, specify SimpleNodeConfig"
                                  Just snbc -> return $ poaOutPort snbc
-
+-}
                     rpc_p   <- try (getEnv "rpcPort") >>= \case
                             Right item              -> return $ read item
                             Left (_::SomeException) -> case simpleNodeBuildConfig conf of
@@ -71,7 +71,7 @@ main =  do
 
                     void $ forkIO $ serveInfoMsg (ConnectInfo stat_h stat_p) (ConnectInfo logs_h logs_p) aInfoCh (toInteger aMyNodeId)
 
-                    void $ forkIO $ servePoA poa_in poa_out aMyNodeId ch aChan aInfoCh
+                    void $ forkIO $ servePoA poa_in aMyNodeId ch aChan aInfoCh
                     void $ forkIO $ serveRpc rpc_p ch aInfoCh
 
                     writeChan aInfoCh $ Metric $ increment "cl.node.count"
