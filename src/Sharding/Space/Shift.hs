d67f8e0360f666960b660be05ab12f05edfc0cb0
mixailflash <mixailflash@gmail.com>
2018-05-28 08:57:18 +0300

    fix

--- src/Sharding/Space/Shift.hs
+++ src/Sharding/Space/Shift.hs
@@ -40,7 +40,7 @@ shiftToCenterOfMass aMyNodePosition aNearestPositions
       | S.size aNearestPositions == 0 = aMyNodePosition
       | S.size aNearestPositions == 1 = moveToOneSixteenth aMyNodePosition (head $ S.elems aNearestPositions)
       | S.size aNearestPositions == 2 = moveToTriangle aMyNodePosition $ S.toList aNearestPositions
-      | S.size aNearestPositions > 3  = moveToSquere aMyNodePosition $ take 3 $ sortOn (\a -> distanceTo aMyNodePosition a) $ S.toList aNearestPositions
+      | S.size aNearestPositions >= 3  = moveToSquere aMyNodePosition $ take 3 $ sortOn (\a -> distanceTo aMyNodePosition a) $ S.toList aNearestPositions
 
 
 
@@ -53,38 +53,16 @@ moveToOneSixteenth (MyNodePosition (Point x1 y1)) (NodePosition (Point x2 y2))
 
 moveToTriangle :: MyNodePosition -> [NodePosition] -> MyNodePosition
 moveToTriangle aMyNodePosition ((NodePosition (Point x1 y1)) : (NodePosition (Point x2 y2) :[])) =
-    if distanceTo aMyNodePosition (NodePosition $ Point xm2 ym2) > distanceTo aMyNodePosition (NodePosition $ Point xm3 ym3)
-    then MyNodePosition (Point xm3 ym3)
-    else MyNodePosition (Point xm2 ym2)
+    if distanceTo aMyNodePosition (NodePosition $ Point xm1 ym1) > distanceTo aMyNodePosition (NodePosition $ Point xm2 ym2)
+    then MyNodePosition (Point xm2 ym2)
+    else MyNodePosition (Point xm1 ym1)
     where
-      xm = (x1 `plusInWorld` x2) `div` 2
-      ym = (y1 `plusInWorld` y2) `div` 2
-      k1 ::Double
-      k1 = ((fromIntegral x1) - (fromIntegral x2)) / ((fromIntegral y1) - (fromIntegral y2))
-      k2 = - 1/k1
-      k3 = 1/k1
-      b :: Word64
-      b = (round $ ((fromIntegral ym) - k2 * (fromIntegral xm))) ::Word64
-
-      xdiff = (fromIntegral x1) - (fromIntegral x2)
-      ydiff = (fromIntegral y1) - (fromIntegral y2)
-      lenLine :: Double
-      lenLine = xdiff**2 + ydiff **2
-      len = if (lenLine < 18446744073709541115/16)
-            then lenLine * 1.2
-            else lenLine
-
-      xm2:: Word64
-      xm2 = minusInWorld (round $ sqrt $ len / (1 + k2 ** 2)) xm
-      --xm2 = - (fromIntegral xm) +
-      xm3:: Word64
-      xm3 = minusInWorld (round $ sqrt $ len / (1 + k3 ** 2)) xm
-      --xm3 = - (fromIntegral xm) + sqrt $ lenLine / (1 + k3 ** 2)
-      ym2::Word64
-      ym2 = plusInWorld (round $ (k2 * (fromIntegral xm2))) b
-      ym3::Word64
-      ym3 = plusInWorld (round $ (k3 * (fromIntegral xm3))) b
-
+      mx = x2 `minusInWorld` x1
+      my = y2 `minusInWorld` y1
+      xm1 = ((div mx 2) `minusInWorld` (div (my*866) 1000) ) `plusInWorld` x1
+      xm2 = ((div mx 2) `plusInWorld`  (div (my*866) 1000) ) `plusInWorld` x1
+      ym1 = ((div my 2) `plusInWorld`  (div (mx*866) 1000) ) `plusInWorld` x1
+      ym2 = ((div my 2) `minusInWorld` (div (mx*866) 1000) ) `plusInWorld` x1
 moveToTriangle a _ = a
 
 moveToSquere :: MyNodePosition -> [NodePosition] -> MyNodePosition
