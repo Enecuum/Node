0c57aef00713c203dc5da1591adc10f55f74fcc9
Ksenia Portu <ksenia.portu@gmail.com>
2018-06-06 13:54:46 +0300

    Add pp code

---
+++ app/PP/Main.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings, LambdaCase, ScopedTypeVariables #-}
+
+module Main where
+
+import Service.Network.WebSockets.Client
+import Network.WebSockets hiding (runClient)
+import Data.ByteString
+
+main :: IO ()
+main = undefined
