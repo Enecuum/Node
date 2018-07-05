9c70eef758dec196638ac8e67be0c1c502235301
dmitriy.pavlyuk <al.lastor@bk.ru>
2018-07-05 21:35:02 +0300

    tester

--- app/Tester/Main.hs
+++ app/Tester/Main.hs
@@ -5,34 +5,47 @@ import PoA.PoAServer
 import Service.Network.WebSockets.Client
 import System.Environment (getArgs)
 import Control.Monad
+import Data.Aeson
 import Control.Concurrent.Async
 import Control.Concurrent
 import qualified Data.Text as T
+import Service.Types
 
 import qualified    Network.WebSockets                  as WS
+import Service.Transaction.TransactionsDAG
 
+data SendTransaction = SendTransaction Transaction
+
+instance ToJSON SendTransaction where
+    toJSON (SendTransaction aTransaction) = object [
+        "tag"           .= ("Request"  :: String),
+        "type"          .= ("PendingAdd"  :: String),
+        "transaction"   .= aTransaction
+      ]
 
 
 main = do
     aArgs <- getArgs
-    if null aArgs then print "Add ip to args!!!"
-    else do
-        let ip = head aArgs
-        runClient ip 1554 "/" socketActor
+    case aArgs of
+        "b":ip:_ -> runClient ip 1554 "/" $ socketActor (\aConnect -> forever $ do
+                threadDelay 1000
+                WS.sendBinaryData aConnect ("{\"tag\": \"Request\", \"type\": \"Broadcast\", \"recipientType\" : \"PoA\", \"msg\" : { \"str\": \"000000000\"}}" :: T.Text))
+        "t":ip:_ -> do
+            aTransactions <- genNTx 1000
+            putStrLn "Transactions generated"
+            runClient ip 1554 "/" $ socketActor $ \aConnect ->
+                forM_ (cycle aTransactions) $ \aTrans -> do
+                    threadDelay 1000
+                    WS.sendBinaryData aConnect $ encode $ SendTransaction aTrans
+        _ -> return ()
 
-
-socketActor :: WS.Connection -> IO ()
-socketActor aConnect = do
-    WS.sendTextData aConnect ("{\"tag\":\"Response\",\"type\": \"NodeId\",\"nodeId\": \"31111111111111111111111111111113\",\"nodeType\" : \"PoA\" }" :: T.Text)
-    void $ race sender (receiver 0)
+socketActor aSender aConnect = do
+    WS.sendTextData aConnect ("{\"tag\":\"Action\", \"type\":\"Connect\", \"node_type\": \"PoA\"}" :: T.Text)
+    void $ race (aSender aConnect) (receiver 0)
   where
-    sender :: IO ()
-    sender = forever $ do
-        threadDelay 1000
-        WS.sendBinaryData aConnect ("{\"tag\": \"Request\", \"type\": \"Broadcast\", \"recipientType\" : \"PoA\", \"msg\" : { \"str\": \"000000000\"}}" :: T.Text)
-
     receiver :: Int -> IO ()
     receiver i = do
-        void $ WS.receiveDataMessage aConnect
+        aMsg <- WS.receiveDataMessage aConnect
         print i
+        when (i`mod`1000 == 0) $ print aMsg
         receiver $ i + 1
