47a4b85fc767a24ac563f7a1d3e075c81ce77ef5
Ksenia Portu <ksenia.portu@gmail.com>
2018-07-23 21:39:40 +0300

    Add genPoAMicroblock function

--- src/Service/Transaction/Test4.hs
+++ src/Service/Transaction/Test4.hs
@@ -3,13 +3,14 @@
 
 module Service.Transaction.Test4 where
 
+import           Control.Monad                       (replicateM)
 import           Control.Monad.State
 import qualified Crypto.Hash.SHA256                  as SHA
 import qualified Data.ByteString.Char8               as BC
+import           Service.Transaction.Storage
 import           Service.Transaction.TransactionsDAG
 import           Service.Types
--- import           Service.Types.PublicPrivateKeyPair
-import           Service.Transaction.Storage
+import           Service.Types.PublicPrivateKeyPair
 import           Service.Types.SerializeJSON         ()
 import           System.Random
 
@@ -33,30 +34,9 @@ genMicroBlockV1 = do
   return (MicroblockV1 aHashCurrentMicroblock aHashPreviousMicroblock tx)
 
 
--- genPoWKeyBlocks :: StateT (Integer,HashOfKeyBlock) IO KeyBlockInfoPoW
-genPoWKeyBlocks :: StateT HashOfKeyBlock IO KeyBlockInfoPoW
+
+genPoWKeyBlocks :: StateT (Integer,HashOfKeyBlock) IO KeyBlockInfoPoW
 genPoWKeyBlocks = do
-  -- (aNumber, prev_hash) <- get
-  prev_hash <- get
-  let aTime = 0
-      aNonce = 0
-      aType = 0
-      aSolver = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-      key = KeyBlockInfoPoW{
-        _time = aTime,
-        _prev_hash = prev_hash,
-        _number = 0, --aNumber,
-        _nonce = aNonce,
-        _solver = aSolver,
-        _type = aType}
-      currentHash = getKeyBlockHash key
-  put currentHash
-  -- put (number + 1, currentHash)
-  return key
-
-
-genPoWKeyBlocks2 :: StateT (Integer,HashOfKeyBlock) IO KeyBlockInfoPoW
-genPoWKeyBlocks2 = do
    (aNumber, prev_hash) <- get
    let aTime = 0
        aNonce = 0
@@ -74,45 +54,29 @@ genPoWKeyBlocks2 = do
    return key
 
 
--- [
---         {
---             "time": 0,
---             "nonce": 0,
---             "number": 0,
---             "type": 0,
---             "prev_hash": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
---             "solver": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
---         },
---         {
---             "time": 1532346450,
---             "nonce": 753983,
---             "number": 1,
---             "type": 0,
---             "prev_hash": "B1Vh7/LNOtWGd2+pBPAEAoLF9qJh9qj9agpSTRTNLSw=",
---             "solver": "OvS8LmmcMa4mtEWbifO5ZFkqT6AYRizzQ6mEobMMhz4="
---         },
---         {
---             "time": 1532346451,
---             "nonce": 986562,
---             "number": 2,
---             "type": 0,
---             "prev_hash": "AAABUnZe66jFjkvc1Nr6npBcZhZkauKL0+t+XgtqMRo=",
---             "solver": "OvS8LmmcMa4mtEWbifO5ZFkqT6AYRizzQ6mEobMMhz4="
---         },
---         {
---             "time": 1532346452,
---             "nonce": 392463,
---             "number": 3,
---             "type": 0,
---             "prev_hash": "AAABDXmD7qViron3Sl7l8SWWAi0KYEFEnPqvjMEJf4c=",
---             "solver": "OvS8LmmcMa4mtEWbifO5ZFkqT6AYRizzQ6mEobMMhz4="
---         },
---         {
---             "time": 1532346459,
---             "nonce": 1155359,
---             "number": 4,
---             "type": 0,
---             "prev_hash": "AAAAjZDpfGOLG38KrYn+ON6y69vZ5Y5NV9UQXBwHe8k=",
---             "solver": "OvS8LmmcMa4mtEWbifO5ZFkqT6AYRizzQ6mEobMMhz4="
---         }
---     ]
+generatePoWKeyBlock :: Int -> IO [KeyBlockInfoPoW]
+generatePoWKeyBlock n = evalStateT (replicateM n genPoWKeyBlocks) (0, hashOfgenesis)
+
+
+hashOfgenesis :: HashOfKeyBlock
+hashOfgenesis = "B1Vh7/LNOtWGd2+pBPAEAoLF9qJh9qj9agpSTRTNLSw="
+
+
+genPoAMicroblock :: HashOfKeyBlock -> IO Microblock
+genPoAMicroblock h = do
+  tx <- genNNTx 5
+  (KeyPair pubKey privateKey) <- generateNewRandomAnonymousKeyPair
+  let aPublisher = pubKey
+  aSign <- getSignature privateKey ("Secret message" :: String)
+  keys <- replicateM 3 generateNewRandomAnonymousKeyPair
+  let aTeamkeys = map (\(KeyPair p _) -> p) keys
+  return Microblock{
+  _keyBlock = h,
+  _sign = aSign,
+  _teamKeys = aTeamkeys,
+  _publisher = aPublisher,
+  _transactions = tx}
+
+
+generateMicroblocksAndKeyBlocks :: Int -> IO (KeyBlockInfoPoW, [Microblock])
+generateMicroblocksAndKeyBlocks = undefined
