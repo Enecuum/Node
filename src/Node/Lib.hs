46d839a32424c60c8c71f0889a10d10aecf84e5b
Dmitrij.Pavluk <al.lastor@bk.ru>
2018-07-24 10:30:51 +0300

    fix of bug

--- src/Node/Lib.hs
+++ src/Node/Lib.hs
@@ -110,7 +110,7 @@ connectManager
 
 connectManager aSyncChan (inDBActorChan, _) aManagerChan aPortNumber aBNList aConnectsChan aMyNodeId inChanPending aInfoChan = do
     writeLog aInfoChan [ConnectingTag, InitTag] Info "Manager of connecting started."
-    void $ syncServer aSyncChan inDBActorChan aManagerChan aInfoChan
+    void $ C.forkIO $ syncServer aSyncChan inDBActorChan aManagerChan aInfoChan
     forM_ aBNList $ \(Connect aBNIp aBNPort) -> do
         void . C.forkIO $ runClient (showHostAddress aBNIp) (fromEnum aBNPort) "/" $ \aConnect -> do
             WS.sendTextData aConnect . encode $ ActionAddToConnectList aPortNumber
