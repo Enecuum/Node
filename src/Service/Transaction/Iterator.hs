6b2d3dac74851f45d3830b29b6b96e79a7d0ddfd
Ksenia Portu <ksenia.portu@gmail.com>
2018-08-02 17:06:24 +0300

    Modify get last values with offset

--- src/Service/Transaction/Iterator.hs
+++ src/Service/Transaction/Iterator.hs
@@ -8,7 +8,7 @@ import           Control.Monad                       (replicateM, when)
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Resource
 import           Control.Monad.Trans.State           (StateT, evalStateT, get,
-                                                      put)
+                                                      put, runStateT)
 -- import           Data.Maybe
 -- import qualified Control.Concurrent                  as C
 import           Control.Exception                   (throw)
@@ -23,136 +23,50 @@ import           Service.Types
 import           Service.Types.PublicPrivateKeyPair
 
 
-getNFirstValuesT :: StateT Rocks.Iterator IO DBValue
-getNFirstValuesT = do
-  it <- get
-  Just v <- Rocks.iterValue it
-  Rocks.iterNext it
-  put it
-  return v
-
-
-getNLastValuesT :: StateT Rocks.Iterator IO DBValue
+getNLastValuesT :: StateT Rocks.Iterator IO (Maybe DBValue)
 getNLastValuesT = do
   it <- get
   value <- Rocks.iterValue it
   Rocks.iterPrev it
   put it
-  case value of
-    Nothing -> throw NoValueInDBAnymore
-    Just j  -> return j
-
-
-findLastValue :: Rocks.Iterator -> IO (Maybe DBValue)
-findLastValue it = do
-  Rocks.iterLast it
-  v <- Rocks.iterValue it
-  Rocks.iterPrev it
-  return v
-
-
-findUntil2 :: StateT (Rocks.Iterator, Int, Int, DBValue -> Bool) IO (Maybe DBValue)
-findUntil2  = do
-  (it, count, maximum', predicate) <- get
-  if count > 0 && maximum' > 0
-    then do
-    -- print $ "count " ++ show count ++ " maximum' " ++ show maximum'
-    Rocks.iterLast it
-    rawTx <- Rocks.iterValue it
-    Rocks.iterPrev it
-    case rawTx of
-      Nothing -> return Nothing
-      Just j  -> if predicate j
-        then do
-        put (it, count - 1, maximum' - 1, predicate)
-        -- rest <- findUntil2 it (count - 1) (maximum' - 1) predicate
-        -- return (j:rest)
-        return $ Just j
-        else do
-        put (it, count, maximum' - 1, predicate)
-        -- rest <- findUntil2
-        return Nothing
-    else return Nothing
+  return value
 
 
 findUntil :: Rocks.Iterator -> Int -> Int -> (DBValue -> Bool) -> IO [DBValue]
 findUntil it count maximum' predicate = if count > 0 && maximum' > 0
     then do
     print $ "count " ++ show count ++ " maximum' " ++ show maximum'
-    Rocks.iterLast it
-    rawTx <- Rocks.iterValue it
-    Rocks.iterPrev it
-    -- rawTx <- evalStateT getNLastValuesT it
+    (rawTx, newIter) <- runStateT getNLastValuesT it
     case rawTx of
-      Nothing -> return []
+      Nothing -> print "Nothing" >> return []
       Just j  -> if predicate j
         then do
-        rest <- findUntil it (count - 1) (maximum' - 1) predicate
+        rest <- findUntil newIter (count - 1) (maximum' - 1) predicate
         return (j:rest)
-        else findUntil it count (maximum' - 1) predicate
-    else return []
+        else findUntil newIter count (maximum' - 1) predicate
+    else print "out of bounds" >> return []
 
 
-g12 = runResourceT $ do
-  let path = superDBPath
-  (_, db) <- Rocks.openBracket path def{Rocks.createIfMissing=False}
-  it <- Rocks.iterOpen db Rocks.defaultReadOptions
-  lift $ evalStateT (replicateM 3 findUntil2) (it, 3, 3, const True)
-  -- lift $ findUntil2 it 3 9 (\_ -> True)
-
-
-g11 = runResourceT $ do
-  let path = superDBPath
-  (_, db) <- Rocks.openBracket path def{Rocks.createIfMissing=False}
-  it <- Rocks.iterOpen db Rocks.defaultReadOptions
-  lift $ findUntil it 3 9 (const True)
-
-
--- getNFirstValues :: (MonadTrans t, MonadResource (t IO)) => Rocks.DB -> Int -> t IO [DBValue]
-getNFirstValues :: Rocks.DB -> Int -> IO [DBValue]
-getNFirstValues db n = runResourceT $ do
-  it    <- Rocks.iterOpen db Rocks.defaultReadOptions
-  Rocks.iterFirst it
-  lift $ evalStateT (replicateM n getNFirstValuesT) it
-
-
-getNLastValues :: Rocks.DB -> Int -> IO [DBValue]
+getNLastValues :: Rocks.DB -> Int -> IO [Maybe DBValue]
 getNLastValues db n = runResourceT $ do
   it    <- Rocks.iterOpen db Rocks.defaultReadOptions
   Rocks.iterLast it
   lift $ evalStateT (replicateM n getNLastValuesT) it
 
 
-getNLastValues2 :: Rocks.DB -> Int -> IO [(DBKey, DBValue)]
-getNLastValues2 db n = runResourceT $ do
-  it    <- Rocks.iterOpen db Rocks.defaultReadOptions
-  Rocks.iterLast it
-  _ <- replicateM (n - 1) $ Rocks.iterPrev it
-  Rocks.iterItems it
-
-
--- getFirst :: (MonadResource (t IO), MonadTrans t) => Rocks.DB -> Int -> Int -> t IO [DBValue]
-getFirst offset db count = drop offset <$> getNFirstValues db (offset + count )
-
-
--- getLast :: Int -> Rocks.DB ->  Int -> IO [(DBKey, DBValue)]
 getLast offset db count = drop offset <$> getNLastValues db (offset + count )
 
+path1 :: FilePath
+path1 = "/tmp/haskell-rocksDB18"
 
-findTransactionsForWallet1 :: Rocks.DB -> PublicKey -> Int -> Int -> IO [TransactionAPI]
-findTransactionsForWallet1 db pubKey offset aCount = runResourceT $ do
-  it    <- Rocks.iterOpen db Rocks.defaultReadOptions
-  lift $ findTransactionsForWallet it pubKey aCount 10
+path2 :: FilePath
+path2 = "/tmp/txInfo18"
 
-
-
-superDBPath :: FilePath
-superDBPath = "/tmp/haskell-rocksDB17"
-
+superPubKey = read "KX8GHxYaejRMEmDrM7ZrbMudvKY57rwW1u4PZN9s3z1z" :: PublicKey
 
 test07 :: IO ()
 test07 = do
-  let path = superDBPath
+  let path = path1
   db <- Rocks.open path def{Rocks.createIfMissing=True}
   Rocks.write db def{Rocks.sync = True} [ Rocks.Put "-" "zero"]
   Rocks.write db def{Rocks.sync = True} [ Rocks.Put "a" "one"
@@ -166,11 +80,8 @@ test07 = do
   Rocks.write db def{Rocks.sync = True} [ Rocks.Put "j" "nine"]
   Rocks.close db
 
-
-
-
 go fun n = do
-  let path = superDBPath
+  let path = path1
   db <- Rocks.open path def{Rocks.createIfMissing=True}
   -- m <- getNLastValues db n
   m <- fun db n
@@ -178,18 +89,10 @@ go fun n = do
   Rocks.close db
 
 
-g1 = go getNLastValues2 2
-g2 = go (getFirst 0) 2
-g3 = go (getLast 0) 3
+
+
 g21 = go (getLast 0) 21
-
-
-
-findTransactionsForWallet :: Rocks.Iterator -> PublicKey -> Int -> Int -> IO [TransactionAPI]
-findTransactionsForWallet it pubKey count maximum' = do
-  txInfo <- findUntil it count maximum' (decodeAndFilter pubKey)
-  let tx = map (\t -> _tx (decodeThis "TransactionInfo" t :: TransactionInfo)) txInfo
-  return $ map (\t -> TransactionAPI { _tx = t, _txHash = rHashT t}) tx
+g3 = go (getLast 0) 12
 
 
 testTxForWallet :: IO ()
@@ -198,48 +101,91 @@ testTxForWallet = do
   tx <- genNTx 6
   -- print tx
   -- print $ length tx
-  let pubKey = read "KX8GHxYaejRMEmDrM7ZrbMudvKY57rwW1u4PZN9s3z1z" :: PublicKey
+  let pubKey = superPubKey
       txKV = zip [1..] tx
       funTx (k,t)
         | even k = t {_owner = pubKey }
         | odd k = t
-        | otherwise = error "There exist numbers which are not even or odd"
+        | otherwise = error "Exist numbers which are not even or odd!"
       mTx = map funTx txKV
   -- print txKV
   -- print mTx
-  print $ map (txFilterByKey pubKey) mTx
+      isGood = map (txFilterByKey pubKey) mTx
+  print $ "Is key there: " ++ (show isGood)
+  print $ length $ filter (==True) $ isGood
   let ti t = TransactionInfo t (BC.pack "123") 2 False
       kv = map (\t -> (rHashT t, S.encode $ ti t)) mTx
       kvB = map (uncurry Rocks.Put) kv
-      path = "/tmp/txInfo18"
+      path = path2
 
   db <- Rocks.open path def{Rocks.createIfMissing=True}
   -- write tx to db
   Rocks.write db def{Rocks.sync = True} kvB
 
   -- read tx from db
-  -- result <- mapM (Rocks.get db  Rocks.defaultReadOptions) (map fst kv)
-  -- print $ map (\r -> (decodeRaw r) :: Maybe TransactionInfo) result
-
-  -- find values in db
-  what <- findTransactionsForWallet1 db pubKey 0 3
-  print $ "we have found: " ++ show (length what) ++ "Transactions"
+  result <- mapM (Rocks.get db  Rocks.defaultReadOptions) (map fst kv)
+  print $ "In DB transactions: "
+  print $ length $ map (\r -> (decodeRaw "TransactionInfo" r) :: Maybe TransactionInfo) result
+  print "----------------------------------------"
   Rocks.close db
 
 
-instance Show Rocks.Iterator where
-  show _ = "Iterator"
 
 
-kvOffset :: IO (M.Map (PublicKey, Integer) Rocks.Iterator)
-kvOffset = runResourceT $ do
-  let n = 5
-  keys <- lift $ replicateM n generateNewRandomAnonymousKeyPair
-  let pubKeys = map (\(KeyPair pub _) -> pub ) keys
-  let path = superDBPath
-  (_, db) <- Rocks.openBracket path def{Rocks.createIfMissing=True}
-  it <- Rocks.iterOpen db Rocks.defaultReadOptions
-  let iter = replicate n it
-  -- iter <- lift $ replicateM n (\i -> do {Rocks.iterPrev i; return i}) it
-  let kv = zip (zip pubKeys [1..]) iter
-  return $ M.fromList kv
+
+decodeTx :: [DBValue] -> IO ()
+decodeTx txInfo = do
+  let fun1 t = _tx (decodeThis "TransactionInfo" t :: TransactionInfo)
+  let fun2 t = TransactionAPI { _tx = t, _txHash = rHashT t}
+  let what = map (fun2 . fun1) txInfo
+  print $ "we have found: " ++ show (length what) ++ "Transactions"
+  print what
+
+
+
+
+-- instance Show Rocks.Iterator where
+--   show _ = "Iterator"
+
+
+-- kvOffset :: IO (M.Map (PublicKey, Integer) Rocks.Iterator)
+-- kvOffset = runResourceT $ do
+--   let n = 5
+--   keys <- lift $ replicateM n generateNewRandomAnonymousKeyPair
+--   let pubKeys = map (\(KeyPair pub _) -> pub ) keys
+--   let path = path1
+--   (_, db) <- Rocks.openBracket path def{Rocks.createIfMissing=True}
+--   it <- Rocks.iterOpen db Rocks.defaultReadOptions
+--   let iter = replicate n it
+--   -- iter <- lift $ replicateM n (\i -> do {Rocks.iterPrev i; return i}) it
+--   let kv = zip (zip pubKeys [1..]) iter
+--   return $ M.fromList kv
+
+
+-- g2 = go (getFirst 0) 2
+-- getNFirstValuesT :: StateT Rocks.Iterator IO DBValue
+-- getNFirstValuesT = do
+--   it <- get
+--   Just v <- Rocks.iterValue it
+--   Rocks.iterNext it
+--   put it
+--   return v
+
+-- getNFirstValues :: Rocks.DB -> Int -> IO [DBValue]
+-- getNFirstValues db n = runResourceT $ do
+--   it    <- Rocks.iterOpen db Rocks.defaultReadOptions
+--   Rocks.iterFirst it
+--   lift $ evalStateT (replicateM n getNFirstValuesT) it
+
+
+-- getFirst offset db count = drop offset <$> getNFirstValues db (offset + count )
+
+
+-- getNLastValues2 :: Rocks.DB -> Int -> IO [(DBKey, DBValue)]
+-- getNLastValues2 db n = runResourceT $ do
+--   it    <- Rocks.iterOpen db Rocks.defaultReadOptions
+--   Rocks.iterLast it
+--   _ <- replicateM (n - 1) $ Rocks.iterPrev it
+--   Rocks.iterItems it
+
+-- g1 = go getNLastValues2 2
