89d088cb6f83c958582c52f3ff74ff86ba243ed3
Dmitrij Pavluk <al.lastor@bk.ru>
2018-09-03 13:28:57 +0300

    add language

--- src/Enecuum/Dsl/Graph/Interpreter.hs
+++ src/Enecuum/Dsl/Graph/Interpreter.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell#-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module Enecuum.Dsl.Graph.Interpreter where
 
 import           Universum
+import Eff
 import           Enecuum.Dsl.Graph.Language
 
 runGraph :: Eff '[GraphDsl] w -> IO w
