86eb20ed00aca802326c1b58ecad603995531728
Dmitrij Pavluk <al.lastor@bk.ru>
2018-09-13 17:17:51 +0300

    Added logger

--- src/Enecuum/Framework/NodeDefinition/Language.hs
+++ src/Enecuum/Framework/NodeDefinition/Language.hs
@@ -9,6 +9,7 @@ import           Enecuum.Prelude
 import           Enecuum.Core.Language                    ( CoreEffects )
 import           Enecuum.Framework.Node.Language          ( NodeModel, HandlersF )
 import qualified Enecuum.Framework.Domain                 as D
+import           Enecuum.Framework.RpcMethod.Language     (RpcMethod)
 
 -- | Node description language.
 -- Allows to specify what actions should be done when node starts.
@@ -17,8 +18,11 @@ data NodeDefinitionL a where
   NodeTag        :: D.NodeTag -> NodeDefinitionL ()
   -- | Initialization of the node by evaluating of some node script.
   Initialization :: Eff NodeModel a -> NodeDefinitionL a
-  -- | Serving of RPC requests.
+  -- | Serving of WS connects.
   Serving        :: HandlersF -> NodeDefinitionL ()
+  -- | Serving of Rpc request.
+  ServingRpc     :: [RpcMethod] -> NodeDefinitionL ()
+
 
 makeFreer ''NodeDefinitionL
 
