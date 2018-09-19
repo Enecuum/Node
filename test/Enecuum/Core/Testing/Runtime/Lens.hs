5ad7d48f48198b10754373df50dadbcade6de618
Daria Melnikova <melnikova_daria@inbox.ru>
2018-09-19 14:23:07 +0300

    style fix

--- test/Enecuum/Core/Testing/Runtime/Lens.hs
+++ test/Enecuum/Core/Testing/Runtime/Lens.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TemplateHaskell        #-}
 
 module Enecuum.Core.Testing.Runtime.Lens where
 
-import Enecuum.Prelude (makeFieldsNoPrefix)
+import           Enecuum.Prelude                    (makeFieldsNoPrefix)
 
-import Enecuum.Core.Testing.Runtime.Types ( LoggerRuntime )
+import           Enecuum.Core.Testing.Runtime.Types (LoggerRuntime)
 
 makeFieldsNoPrefix ''LoggerRuntime
