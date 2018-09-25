e840d165af44b063939f022851b162ab98da72c8
Dmitrij Pavluk <al.lastor@bk.ru>
2018-09-25 10:20:32 +0300

    port fixes

--- src/Enecuum/Assets/Nodes/Address.hs
+++ src/Enecuum/Assets/Nodes/Address.hs
@@ -5,5 +5,5 @@ import qualified Enecuum.Domain                as D
 import           Enecuum.Legacy.Service.Network.Base (ConnectInfo (..))
 
 networkNode1Addr, networkNode2Addr :: D.NodeAddress
-networkNode1Addr = ConnectInfo "127.0.0.1" 1001
-networkNode2Addr = ConnectInfo "127.0.0.2" 1002
+networkNode1Addr = ConnectInfo "127.0.0.1" 2001
+networkNode2Addr = ConnectInfo "127.0.0.2" 2002
