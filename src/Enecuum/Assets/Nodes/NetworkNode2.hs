99e5fe62d23006c590ae2e53795604dff25b744d
Dmitrij Pavluk <al.lastor@bk.ru>
2018-09-25 10:26:27 +0300

    fix of adress

--- src/Enecuum/Assets/Nodes/NetworkNode2.hs
+++ src/Enecuum/Assets/Nodes/NetworkNode2.hs
@@ -77,7 +77,7 @@ networkNode2 :: Typeable cfg => TG.GraphVar -> L.NodeDefinitionL cfg ()
 networkNode2 g = do
   L.nodeTag "networkNode2"
   nodeData <- L.initialization $ newtorkNode2Initialization g
-  L.servingRpc 1002 $ do
+  L.servingRpc 2002 $ do
     L.method (acceptGetBalance nodeData)
     L.method (acceptBalanceChange nodeData)
 
