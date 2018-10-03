ba34ca28b91d176e0a2b88eb6227d10950be1765
Ksenia Portu <ksenia.portu@gmail.com>
2018-10-03 16:16:55 +0300

    Extend random language by get random int method

--- src/Enecuum/Core/CoreEffect/Language.hs
+++ src/Enecuum/Core/CoreEffect/Language.hs
@@ -7,17 +7,18 @@ module Enecuum.Core.CoreEffect.Language
 import           Enecuum.Prelude
 
 import           Enecuum.Core.Logger.Language (Logger, LoggerL, logMessage)
-import Enecuum.Core.Random.Language
+import           Enecuum.Core.Random.Language (ERandom, ERandomL, getRandomInt)
 
 -- | Core effects container language.
 data CoreEffectF next where
   -- | Logger effect
   EvalLogger :: LoggerL () -> (() -> next) -> CoreEffectF next
-  EvalRandom :: RandomL g -> (a -> next) -> CoreEffectF next
+  -- | Random effect
+  EvalRandom :: ERandomL g -> (a -> next) -> CoreEffectF next
 
 instance Functor CoreEffectF where
   fmap g (EvalLogger logger next) = EvalLogger logger (g . next)
-  fmap g (EvalRandom gen next) = EvalRandom gen (g . next)
+  fmap g (EvalRandom n next)      = EvalRandom n (g . next)
 
 type CoreEffect next = Free CoreEffectF next
 
@@ -27,9 +28,9 @@ evalLogger logger = liftF $ EvalLogger logger id
 instance Logger (Free CoreEffectF) where
   logMessage level msg = evalLogger $ logMessage level msg
 
-evalRandom :: RandomL a -> CoreEffect ()
+
+-- evalRandom :: ERandomL a -> CoreEffect ()
 evalRandom g = liftF $ EvalRandom g id
 
-instance CRandom (Free CoreEffectF) where
-  -- evalRand r = evalRandom $ evalRand r
-  evalRand r = undefined
+instance ERandom (Free CoreEffectF) where
+  getRandomInt r = evalRandom $ getRandomInt r
--- src/Enecuum/Core/Random/Interpreter.hs
+++ src/Enecuum/Core/Random/Interpreter.hs
@@ -3,9 +3,21 @@ module Enecuum.Core.Random.Interpreter where
 import           Enecuum.Prelude
 import qualified Enecuum.Core.Language       as L
 import qualified Control.Monad.Random.Lazy as R
+import System.Random
 
 -- | Interpret RandomL language.
-interpretRandomL :: L.RandomF a -> IO a
+interpretRandomL :: L.ERandomF a -> IO a
+interpretRandomL (L.GetRandomInt k next) = do
+    r <- randomRIO k
+    pure $ next r
 interpretRandomL (L.EvalRand r g next) = do
     let a = R.evalRand r g
     pure $ next a
+
+runL l = foldFree interpretRandomL l
+
+scenario :: L.ERandomL [Integer]
+scenario = do
+    replicateM 10 $ L.getRandomInt (5,10)
+
+go = runL scenario
