e5f43d72313ae1e6661e44489873446c1ceec437
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-05 15:01:00 +0300

    fix of addres

--- src/Enecuum/Assets/Nodes/Address.hs
+++ src/Enecuum/Assets/Nodes/Address.hs
@@ -4,7 +4,6 @@ import qualified Enecuum.Domain                as D
 
 networkNode1Addr, networkNode2Addr,
   networkNode3Addr, networkNode4Addr :: D.Address
-graphNodeAddr    = D.Address "127.0.0.1" 2001
 networkNode1Addr = D.Address "127.0.0.1" 2001
 networkNode2Addr = D.Address "127.0.0.2" 2002
 networkNode3Addr = D.Address "127.0.0.1" 2003
