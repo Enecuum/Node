afc15674009719578ca2f9d7f0ee060a35ac14dc
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-10 10:30:27 +0300

    fixes to tcp-client realisation

--- src/Enecuum/Framework/Networking/Internal/TCP/Client.hs
+++ src/Enecuum/Framework/Networking/Internal/TCP/Client.hs
@@ -9,8 +9,6 @@ module Enecuum.Framework.Networking.Internal.TCP.Client (
     runClient,
     PortNumber(..),
     HostAddress,
-    openConnect,
-    closeConnect
   ) where
 
 import           Prelude
@@ -22,37 +20,18 @@ import           Control.Exception
 class (Show a) => Hosts a where
     openConnect :: a -> PortNumber -> IO Socket
 
-closeConnect :: Socket -> IO ()
-closeConnect = close
-
 -- | Run a TCP client.
 runClient :: Hosts a => a -> PortNumber -> (Socket -> IO ()) -> IO ()
-runClient aHostAddress aPort aPlainHandler = withSocketsDo $ do
-    aHandle <- (try $ openConnect aHostAddress aPort) >>= \case
-        Right h -> return h
-        Left (err :: SomeException) ->
-            error
-                $  "TCP socket connection exception on "
-                ++ show aHostAddress
-                ++ ":"
-                ++ show aPort
-                ++ " :"
-                ++ show err
-                ++ ""
-    (try $ aPlainHandler aHandle) >>= \case
-        Right _ -> closeConnect aHandle
-        Left (err :: SomeException) ->
-            putStrLn $ "TCP socket exception on " ++ show aHostAddress ++ ":" ++ show aPort ++ " :" ++ show err
+runClient aHostAddress port aPlainHandler = withSocketsDo $ do
+    connection <- openConnect aHostAddress port)
+    finally (aPlainHandler connection) (close connection)
 
 instance Hosts HostAddress where
-    openConnect aHostAdress = openConnect (showHostAddress aHostAdress)
+    openConnect address = openConnect (showHostAddress address)
 
 instance Hosts String where
-    openConnect aHostAdress aPort = do
-        aServerAddr <- head <$> getAddrInfo
-            Nothing
-            (Just aHostAdress)
-            (Just $ show aPort)
-        aSocket <- socket (addrFamily aServerAddr) Stream defaultProtocol
-        connect aSocket $ addrAddress aServerAddr
-        return  aSocket
+    openConnect address port = do
+        connectInfo <- head <$> getAddrInfo Nothing (Just address) (Just $ show port)
+        sock <- socket (addrFamily connectInfo) Stream defaultProtocol
+        connect sock $ addrAddress connectInfo
+        pure sock
