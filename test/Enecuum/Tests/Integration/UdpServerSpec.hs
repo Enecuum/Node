fde290ec04b07c4de7090dbf79676d4412819990
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-11 16:01:18 +0300

    Addition of test for duplex udp.

---
+++ test/Enecuum/Tests/Integration/UdpServerSpec.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+module Enecuum.Tests.Integration.UdpServerSpec where
+
+--
+
+import           Enecuum.Prelude
+
+import           Test.HUnit
+import           Test.Hspec
+import           Test.Hspec.Contrib.HUnit                 ( fromHUnitTest )
+import qualified Enecuum.Language              as L
+
+import           Enecuum.Legacy.Service.Network.Base
+import           Enecuum.Interpreters
+import           Enecuum.Language
+import qualified Enecuum.Runtime as Rt
+import qualified Enecuum.Domain                as D
+import           Enecuum.Framework.Networking.Internal.Udp.Connection
+import qualified Data.Map as M
+
+
+
+-- Tests disabled
+spec :: Spec
+spec = describe "UdpServer" $ fromHUnitTest $ TestList [TestLabel "Ping pong" pingPong]
+
+createNodeRuntime :: IO Rt.NodeRuntime
+createNodeRuntime = Rt.createVoidLoggerRuntime >>= Rt.createCoreRuntime >>= Rt.createNodeRuntime
+
+newtype Ping = Ping Int deriving (Generic, ToJSON, FromJSON)
+newtype Pong = Pong Int deriving (Generic, ToJSON, FromJSON)
+data Succes = Succes    deriving (Generic, ToJSON, FromJSON)
+
+
+pingHandle :: D.UdpConnection -> Ping -> D.UdpConnection -> L.NodeL ()
+pingHandle succConn (Ping i) conn = do
+    when (i < 10) $ L.send conn (Pong $ i + 1)
+    when (i == 10) $ do
+        L.send succConn Succes
+        L.close conn
+
+pongHandle :: D.UdpConnection -> Pong -> D.UdpConnection -> L.NodeL ()
+pongHandle succConn (Pong i) conn = do
+    when (i < 10) $ L.send conn (Ping $ i + 1)
+    when (i == 10) $ do
+        L.send succConn Succes
+        L.close conn
+
+pingPong :: Test
+pingPong = TestCase $ do
+    nr1 <- createNodeRuntime
+    nr2 <- createNodeRuntime
+    void $ forkIO $ do
+        threadDelay 5000
+        runNodeDefinitionL nr1 $ do
+            succConn <- L.open succAdr $ pure ()
+            L.serving serverPort $ do
+                L.udpHandler (pingHandle succConn)
+                L.udpHandler (pongHandle succConn)
+        threadDelay 5000
+        runNodeDefinitionL nr2 $ do
+            succConn <- L.open succAdr $ pure ()
+            conn :: D.UdpConnection <- L.open serverAddr $ do
+                L.udpHandler (pingHandle succConn)
+                L.udpHandler (pongHandle succConn)
+            L.send conn $ Ping 0
+    ok <- succesServer succPort
+    runNodeDefinitionL nr1 $ L.stopServing serverPort
+    assertBool "" ok
+
+succesServer :: PortNumber -> IO Bool
+succesServer port = do
+    mvar <- newEmptyMVar
+    void $ forkIO $ do
+        threadDelay 1000000
+        putMVar mvar False
+    ch <- startServer port (M.singleton (D.toTag Succes) (\_ _ -> putMVar mvar True)) (\_ _ -> pure ())
+    ok <- takeMVar mvar
+    Enecuum.Prelude.atomically $ stopServer ch
+    pure ok
+
+serverPort, succPort :: PortNumber
+serverPort = 4000
+succPort = 5000
+
+serverAddr, succAdr :: D.Address
+serverAddr = D.Address "127.0.0.1" serverPort
+succAdr = D.Address "127.0.0.1" succPort
