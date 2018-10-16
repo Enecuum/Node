1cfcd78f7534658fd75bc497f707aa1527c3c611
Ksenia Portu <ksenia.portu@gmail.com>
2018-10-16 10:52:17 +0300

    Add property-test Base64 serialization for ByteString

--- test/Data/ByteStringSpec.hs
+++ test/Data/ByteStringSpec.hs
@@ -13,6 +13,7 @@ import           Test.Hspec                   (Spec, describe, hspec, it, should
 import           Test.Hspec.Contrib.HUnit     (fromHUnitTest)
 import           Test.HUnit                   (Test (..))
 import           Test.QuickCheck
+import           Test.QuickCheck.Monadic (monadicIO, assert, run)
 
 instance Arbitrary ByteString where
     arbitrary = fmap BS.pack $ arbitrary
@@ -23,12 +24,20 @@ spec = do
     describe "Bytestring spec test" $ fromHUnitTest $ TestList
         [ TestLabel "Verify bytestring json serialization" testRandomByteString]
     describe "Idempotent serialization Properties" $
-        do it "Verify bytestring json serialization" $ property prop_JsonEncoding
+        do
+            it "Verify bytestring json serialization" $ property prop_JsonEncoding
+            it "Verify bytestring Base64 serialization" $ property prop_Base64Encoding
 
 testRandomByteString :: Test
 testRandomByteString = TestCase $ do
     bs <- replicateM 1000 $ I.runERandomL $ L.getRandomByteString =<< L.getRandomInt (1,1000)
     map (A.decode . A.encode) bs `shouldBe` map Just bs
 
-prop_JsonEncoding :: [ByteString] -> Bool
+prop_JsonEncoding :: ByteString -> Bool
 prop_JsonEncoding bs = (A.decode . A.encode) bs == Just bs
+
+prop_Base64Encoding :: ByteString -> Property
+prop_Base64Encoding bs = monadicIO $ do
+    es <- run $ (textToBase64 . base64ToText) bs
+    assert $ es == bs
+
