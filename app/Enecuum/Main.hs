bb231f3d76eed413ee73dbe72e0dd449905760ba
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-24 12:15:38 +0300

    ghc-8.6.1

--- app/Enecuum/Main.hs
+++ app/Enecuum/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           App.Initialize  (initialize)
 import           Enecuum.Config  (withConfig)
---import           System.Environment
+import           System.Environment
 import           Enecuum.Prelude
 
 main :: IO ()
