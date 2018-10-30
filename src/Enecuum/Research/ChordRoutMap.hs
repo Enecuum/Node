542f9a04dc0c84d07deb758174d9dd914bba0a82
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-30 17:08:13 +0300

    Fixing of error in ChordRoutMap struct.

--- src/Enecuum/Research/ChordRoutMap.hs
+++ src/Enecuum/Research/ChordRoutMap.hs
@@ -26,24 +26,28 @@ addToMap hash = M.insert (hashToInteger hash)
 removeFromMap :: Ord a => StringHash -> ChordRoutMap a -> ChordRoutMap a
 removeFromMap hash = M.delete (hashToInteger hash)
 
-findInMap :: Ord a => StringHash -> ChordRoutMap a -> Set a
+findInMap :: Ord a => StringHash -> ChordRoutMap a -> Set (StringHash, a)
 findInMap hash rm = S.fromList $ mapMaybe (\i -> findInMapN i hash rm) [0..hashSize-1]
 
-findInMapR :: Ord a => StringHash -> ChordRoutMap a -> Set a
-findInMapR hash rm = S.fromList $ mapMaybe (\i -> findInMapRN i hash rm) [0..hashSize-1]
-
-findInMapN :: Ord a => Integer -> StringHash -> ChordRoutMap a -> Maybe a
-findInMapN i hash rm = if isJust topElem then topElem else bottomElem
+findInMapN :: Ord a => Integer -> StringHash -> ChordRoutMap a -> Maybe (StringHash, a)
+findInMapN i hash rm = 
+    (\(x, y) -> (integerToHash x, y)) <$>
+    (if isJust topElem then topElem else bottomElem)
     where
-        topElem    = snd <$> M.lookupGE elemKey rm
-        bottomElem = snd <$> M.lookupGE 0 rm
+        topElem    = M.lookupGE elemKey rm
+        bottomElem = M.lookupGE 0 rm
         elemKey    = (hashToInteger hash + 2 ^ i) `mod` elemNumber
 
-findInMapRN :: Ord a => Integer -> StringHash -> ChordRoutMap a -> Maybe a
-findInMapRN i hash rm = if isJust topElem then topElem else bottomElem
+findInMapR :: Ord a => StringHash -> ChordRoutMap a -> Set (StringHash, a)
+findInMapR hash rm = S.fromList $ mapMaybe (\i -> findInMapRN i hash rm) [0..hashSize-1]
+
+findInMapRN :: Ord a => Integer -> StringHash -> ChordRoutMap a -> Maybe (StringHash, a)
+findInMapRN i hash rm = 
+    (\(x, y) -> (integerToHash x, y)) <$>
+    (if isJust topElem then topElem else bottomElem)
     where
-        topElem    = snd <$> M.lookupGE elemKey rm
-        bottomElem = snd <$> M.lookupGE 0 rm
+        topElem    = M.lookupGE elemKey rm
+        bottomElem = M.lookupGE 0 rm
         elemKey    = (elemNumber + hashToInteger hash - 2 ^ i) `mod` elemNumber
 
 findNext :: Ord a => StringHash -> ChordRoutMap a -> Maybe a
