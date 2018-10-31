09f1298dfd8ac3588fbd05d423d54370e2964672
Dmitrij Pavluk <al.lastor@bk.ru>
2018-10-31 08:39:55 +0300

    Addition of doocumentation

--- src/Enecuum/Research/ChordRouteMap.hs
+++ src/Enecuum/Research/ChordRouteMap.hs
@@ -12,28 +12,36 @@ import qualified Data.Map                      as M
 import qualified Data.Set                      as S
 import           Data.HGraph.StringHashable
 
+-- | Route map for chord algorithm.
 type ChordRouteMap a = M.Map Integer a
 
+-- | Size of hashes.
 hashSize :: Integer
 hashSize = 256
 
-elemNumber :: Integer
-elemNumber = 2 ^ hashSize
+-- | Quantity of hashes.
+quantityOfHashes :: Integer
+quantityOfHashes = 2 ^ hashSize
 
+-- | Add elem to route map.
 addToMap :: Ord a => StringHash -> a -> ChordRouteMap a -> ChordRouteMap a
 addToMap hash = M.insert (hashToInteger hash)
 
+-- | Remove elem from route map. 
 removeFromMap :: Ord a => StringHash -> ChordRouteMap a -> ChordRouteMap a
 removeFromMap hash = M.delete (hashToInteger hash)
 
+-- | Find all fingers in rout map by straight formulas.
 findInMap :: Ord a => StringHash -> ChordRouteMap a -> Set (StringHash, a)
 findInMap = findInMapByKey
-    (\hash i -> (hashToInteger hash + 2 ^ i) `mod` elemNumber)
+    (\hash i -> (hashToInteger hash + 2 ^ i) `mod` quantityOfHashes)
 
+-- | Find all fingers in rout map by inverse formulas.
 findInMapR :: Ord a => StringHash -> ChordRouteMap a -> Set (StringHash, a)
 findInMapR = findInMapByKey
-    (\hash i -> (elemNumber + hashToInteger hash - 2 ^ i) `mod` elemNumber)
+    (\hash i -> (quantityOfHashes + hashToInteger hash - 2 ^ i) `mod` quantityOfHashes)
 
+-- | Find all fingers in rout map by formulas.
 findInMapByKey
     :: Ord a
     => (StringHash -> Integer -> Integer)
@@ -41,24 +49,26 @@ findInMapByKey
     -> ChordRouteMap a
     -> Set (StringHash, a)
 findInMapByKey elemKey hash rm = S.fromList $ mapMaybe
-    (\i -> findInMapByKeyN elemKey i hash rm) [0..hashSize-1]
+    (\i -> findInMapNByKey elemKey i hash rm) [0..hashSize-1]
 
-findInMapByKeyN
+-- | Find N finger in rout map by formulas.
+findInMapNByKey
     :: (StringHash -> Integer -> Integer)
     -> Integer
     -> StringHash
     -> Map Integer b
     -> Maybe (StringHash, b)
-findInMapByKeyN elemKey i hash rm = 
+findInMapNByKey elemKey i hash rm = 
     (\(x, y) -> (integerToHash x, y)) <$>
     (if isJust bottomElem then bottomElem else topElem)
     where
-        topElem    = M.lookupLE elemNumber rm
+        topElem    = M.lookupLE quantityOfHashes rm
         bottomElem = M.lookupLE (elemKey hash i) rm
 
+-- | Find the closest elem to hash from rout map.
 findNext :: Ord a => StringHash -> ChordRouteMap a -> Maybe a
 findNext hash rm = if isJust bottomElem then bottomElem else topElem
     where
         bottomElem = snd <$> M.lookupLE elemKey rm
-        topElem    = snd <$> M.lookupLE elemNumber rm
+        topElem    = snd <$> M.lookupLE quantityOfHashes rm
         elemKey    = hashToInteger hash
