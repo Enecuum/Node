fb121f2294d3bde9430f3647af26cadf57a430a1
Alexander Granin <graninas@gmail.com>
2018-11-06 14:44:49 +0700

    DB Test diagnostics

--- test/Enecuum/Tests/Scenarios/GraphNodeDBSpec.hs
+++ test/Enecuum/Tests/Scenarios/GraphNodeDBSpec.hs
@@ -41,32 +41,41 @@ dumpAndRestoreGraphTest = do
 
     TestCase $ withDbAbsence dbPath $ withNodesManager $ \mgr -> do
         -- Starting nodes.
+        print @Text "Starting GraphNodeTransmitter"
         transmitterNode1 <- startNode loggerCfg mgr $ A.graphNodeTransmitter cfg
         waitForNode A.graphNodeTransmitterRpcAddress
 
+        print @Text "Starting PoW"
         powNode <- startNode loggerCfg mgr A.powNode
         waitForNode A.powNodeRpcAddress
 
+        print @Text "Requesting last KBlock"
         -- Checking there are none blocks.
         Right topKBlock0 :: Either Text D.KBlock <- makeIORpcRequest A.graphNodeTransmitterRpcAddress A.GetLastKBlock
         topKBlock0 ^. Lens.number `shouldBe` 0
 
+        print @Text "Generating some blocks and checking they are generated"
         -- Generating some blocks and checking they are generated.
         _ :: Either Text A.SuccessMsg <- makeIORpcRequest A.powNodeRpcAddress $ A.NBlockPacketGeneration blocksCount blocksDelay
         waitForBlocks blocksCount A.graphNodeTransmitterRpcAddress
         
+        print @Text "Requesting last KBlock"
         Right topKBlock1 :: Either Text D.KBlock <- makeIORpcRequest A.graphNodeTransmitterRpcAddress A.GetLastKBlock
         topKBlock1 ^. Lens.number `shouldBe` blocksCount
 
+        print @Text "Requesting to dump blocks."
         -- Requesting to dump blocks.
         _ :: Either Text A.SuccessMsg <-  makeIORpcRequest A.graphNodeTransmitterRpcAddress A.DumpToDB
 
-        threadDelay $ 1000 * 1000
+        -- print @Text "Waiting"
+        -- threadDelay $ 1000 * 1000
 
+        print @Text "Stopping nodes"
         -- Stopping nodes, clearing graph.
         stopNode mgr transmitterNode1
         stopNode mgr powNode
 
+        print @Text "Starting node, checking there are no blocks."
         -- Starting node, checking there are no blocks.
         void $ startNode loggerCfg mgr $ A.graphNodeTransmitter cfg
         waitForNode A.graphNodeTransmitterRpcAddress
@@ -74,10 +83,12 @@ dumpAndRestoreGraphTest = do
         Right genesisKBlock :: Either Text D.KBlock <- makeIORpcRequest A.graphNodeTransmitterRpcAddress A.GetLastKBlock
         genesisKBlock `shouldBe` D.genesisKBlock
 
+        print @Text "Requesting to restore blocks from DB and checking they are restored."
         -- Requesting to restore blocks from DB and checking they are restored.
         _ :: Either Text A.SuccessMsg <- makeIORpcRequest A.graphNodeTransmitterRpcAddress A.RestoreFromDB
         waitForBlocks blocksCount A.graphNodeTransmitterRpcAddress
 
+        print @Text "Requesting last KBlock"
         Right topKBlock2 :: Either Text D.KBlock <- makeIORpcRequest A.graphNodeTransmitterRpcAddress A.GetLastKBlock
         topKBlock1 `shouldBe` topKBlock2
 
