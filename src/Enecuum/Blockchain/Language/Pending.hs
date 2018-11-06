f6d65354e1d5aeb4684097846da7b28055064e7f
Alexander Granin <graninas@gmail.com>
2018-11-06 23:43:09 +0700

    StateLog replaced by DelayedLog with the traditional interface.

--- src/Enecuum/Blockchain/Language/Pending.hs
+++ src/Enecuum/Blockchain/Language/Pending.hs
@@ -9,15 +9,14 @@ import qualified Enecuum.Core.Types                       as D
 import qualified Enecuum.Core.Language                    as L
 import qualified Enecuum.Framework.Domain                 as D
 import qualified Enecuum.Framework.Language               as L
-import qualified Enecuum.Framework.LogState               as Log
 import           Enecuum.Prelude
 
 -- | Checks if the kBlock valid.
 -- N.B.: it doesn't support forks.
 -- TODO: support forks.
-validateKBlock :: D.StateVar [Text] -> BlockchainData -> D.KBlock -> L.StateL D.KBlockValidity
-validateKBlock logV bData kBlock = do
-    topKBlock <- L.getTopKeyBlock logV bData
+validateKBlock :: BlockchainData -> D.KBlock -> L.StateL D.KBlockValidity
+validateKBlock bData kBlock = do
+    topKBlock <- L.getTopKeyBlock bData
 
     let nextBlock    = L.kBlockIsNext kBlock topKBlock
     let prevBlock    = L.kBlockExists kBlock topKBlock
@@ -32,35 +31,35 @@ data KBlockResult
     | KBlockPostponed
     | KBlockErrored
 
-addTopKBlock' :: D.StateVar [Text] -> D.BlockchainData -> D.KBlock -> L.StateL KBlockResult
-addTopKBlock' logV bData kBlock = do
-    res <- L.addTopKBlock "[Network]" logV bData kBlock
+addTopKBlock' :: D.BlockchainData -> D.KBlock -> L.StateL KBlockResult
+addTopKBlock' bData kBlock = do
+    res <- L.addTopKBlock "[Network]" bData kBlock
     pure $ if res then KBlockAdded else KBlockErrored
 
 -- | If KBlock is valid, adds it to the graph or to the pending.
 -- If it's not valid (for example, exists already), drops it.
 -- Returns True if Pending needs to be processed.
-addKBlock :: D.StateVar [Text] -> BlockchainData -> D.KBlock -> L.NodeL Bool
-addKBlock logV bData kBlock = do
+addKBlock :: BlockchainData -> D.KBlock -> L.NodeL Bool
+addKBlock bData kBlock = do
     
     res <- L.atomically $ do
-        validity <- validateKBlock logV bData kBlock
+        validity <- validateKBlock bData kBlock
         case validity of
-            D.NextKBlock   -> addTopKBlock' logV bData kBlock
+            D.NextKBlock   -> addTopKBlock' bData kBlock
             D.FutureKBlock -> pure KBlockPostponed
             _              -> pure KBlockErrored
     
     case res of
         KBlockErrored   -> L.logError ("Error in processing KBlock: " +|| kBlock ||+ ".") >> pure False
         KBlockAdded     -> pure True
-        KBlockPostponed -> L.atomically (addBlockToPending logV bData kBlock) >> pure False
+        KBlockPostponed -> L.atomically (addBlockToPending bData kBlock) >> pure False
 
 
 -- | Move one block from pending to graph if possible and remove it from pending.
 --   Return true if function had effect.
-processKBlockPending :: D.StateVar [Text] -> BlockchainData -> L.NodeL Bool
-processKBlockPending logV bData = L.atomically $ do
-    topKBlock <- L.getTopKeyBlock logV bData
+processKBlockPending :: BlockchainData -> L.NodeL Bool
+processKBlockPending bData = L.atomically $ do
+    topKBlock <- L.getTopKeyBlock bData
     pending   <- L.readVar (_kBlockPending bData)
 
     let nextKBlockNumber = topKBlock ^. Lens.number + 1
@@ -69,17 +68,17 @@ processKBlockPending logV bData = L.atomically $ do
         Just kBlock -> do
             let newPending = Map.delete nextKBlockNumber pending
             L.writeVar (_kBlockPending bData) newPending
-            L.addTopKBlock "[Pending]" logV bData kBlock
+            L.addTopKBlock "[Pending]" bData kBlock
 
 -- | Add new key block to pending.
-addBlockToPending :: D.StateVar [Text] -> BlockchainData -> D.KBlock -> L.StateL ()
-addBlockToPending logV bData kBlock = do
-    Log.stateLog logV $ "Adding KBlock to pending: " +|| kBlock ||+ ""
+addBlockToPending :: BlockchainData -> D.KBlock -> L.StateL ()
+addBlockToPending bData kBlock = do
+    L.logInfo $ "Adding KBlock to pending: " +|| kBlock ||+ ""
     L.modifyVar (_kBlockPending bData) (Map.insert (kBlock ^. Lens.number) kBlock)
 
 -- | Add new transaction to pending.
-addTransactionToPending :: D.StateVar [Text] -> BlockchainData -> D.Transaction -> L.StateL Bool
-addTransactionToPending logV bData transaction = do
-    Log.stateLog logV "Add transaction to pending"
-    L.modifyVar (_transactionPending bData) ( Map.insert (D.toHash transaction) transaction )
+addTransactionToPending :: BlockchainData -> D.Transaction -> L.StateL Bool
+addTransactionToPending bData transaction = do
+    L.logInfo "Add transaction to pending"
+    L.modifyVar (_transactionPending bData) (Map.insert (D.toHash transaction) transaction)
     pure True
--- src/Enecuum/Core/Runtime.hs
+++ src/Enecuum/Core/Runtime.hs
@@ -2,30 +2,64 @@ module Enecuum.Core.Runtime where
 
 import Enecuum.Prelude
 
-import qualified Enecuum.Core.Types as T
+import qualified Data.Map                          as Map
+import qualified Data.ByteString.Base64            as Base64
+import qualified Crypto.Hash.SHA256                as SHA
+
+import qualified Enecuum.Core.Types                as D
 import qualified Enecuum.Core.Logger.Impl.HsLogger as Impl
 
-data LoggerRuntime = LoggerRuntime
+newtype LoggerRuntime = LoggerRuntime
     { _hsLoggerHandle :: Maybe Impl.HsLoggerHandle
     }
 
 data CoreRuntime = CoreRuntime
     { _loggerRuntime :: LoggerRuntime
+    , _stateRuntime  :: StateRuntime
+    }
+
+newtype VarNumber = VarNumber Int
+
+data VarHandle = VarHandle D.VarId (TVar Any)
+
+instance D.StringHashable VarNumber where
+  toHash (VarNumber n) = D.StringHash . Base64.encode . SHA.hash $ show ("VarNumber " +|| n ||+ "" :: String)
+
+data DelayedLogEntry = DelayedLogEntry D.LogLevel D.Message
+type DelayedLog = [DelayedLogEntry]
+
+data StateRuntime = StateRuntime
+    { _state      :: TMVar (Map.Map D.VarId VarHandle) -- ^ Node state.
+    , _idCounter  :: TMVar Int                         -- ^ ID counter. Used to generate VarIds, ProcessIds.
+    , _delayedLog :: TVar DelayedLog                   -- ^ Delayed log entries
     }
 
 -- TODO: make it right
 createVoidLoggerRuntime :: IO LoggerRuntime
 createVoidLoggerRuntime = pure $ LoggerRuntime Nothing
 
-createLoggerRuntime :: T.LoggerConfig -> IO LoggerRuntime
+createLoggerRuntime :: D.LoggerConfig -> IO LoggerRuntime
 createLoggerRuntime config = LoggerRuntime . Just <$> Impl.setupLogger config
 
 clearLoggerRuntime :: LoggerRuntime -> IO ()
 clearLoggerRuntime (LoggerRuntime (Just hsLogger)) = Impl.teardownLogger hsLogger
 clearLoggerRuntime _                               = pure ()
 
+createStateRuntime :: IO StateRuntime
+createStateRuntime = StateRuntime
+    <$> newTMVarIO Map.empty
+    <*> newTMVarIO 0
+    <*> newTVarIO []
+
 createCoreRuntime :: LoggerRuntime -> IO CoreRuntime
-createCoreRuntime = pure . CoreRuntime
+createCoreRuntime loggerRt = CoreRuntime loggerRt
+    <$> createStateRuntime
 
 clearCoreRuntime :: CoreRuntime -> IO ()
 clearCoreRuntime _ = pure ()
+
+getNextId :: StateRuntime -> STM Int
+getNextId stateRt = do
+    number <- takeTMVar $ _idCounter stateRt
+    putTMVar (_idCounter stateRt) $ number + 1
+    pure number
