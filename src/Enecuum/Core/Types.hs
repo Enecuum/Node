ed4b16c839f2d6b37fbc7a45810f8cfaabc90975
Alexander Granin <graninas@gmail.com>
2018-11-08 16:07:12 +0700

    Types exposing added.

--- src/Enecuum/Core/Types.hs
+++ src/Enecuum/Core/Types.hs
@@ -7,3 +7,5 @@ import Enecuum.Core.HGraph.Types          as X
 import Enecuum.Core.Types.Logger          as X
 import Enecuum.Core.Types.Database        as X
 import Enecuum.Core.HGraph.Internal.Types as X
+import Enecuum.Core.Crypto.Crypto         as X (PublicKey(..), PrivateKey(..), KeyPair (..))
+import Crypto.PubKey.ECC.ECDSA            as X (Signature)
