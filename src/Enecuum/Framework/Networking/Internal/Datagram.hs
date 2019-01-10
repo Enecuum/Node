ff460b1acbd4cf63d69b6543b73643fbd2bb934a
Dmitrij Pavluk <al.lastor@bk.ru>
2019-01-10 18:27:09 +0300

    fix to datagram reading

--- src/Enecuum/Framework/Networking/Internal/Datagram.hs
+++ src/Enecuum/Framework/Networking/Internal/Datagram.hs
@@ -4,7 +4,15 @@ import           Enecuum.Prelude
 import           Data.Serialize
 import qualified Network.Socket.ByteString.Lazy                   as S
 import qualified Network.Socket                                   as S hiding (recv, send)
+import           Control.Monad.Extra
 
+loopM :: Monad m => (a -> m (Either a b)) -> a -> m b
+loopM act x = do
+    res <- act x
+    case res of
+        Left x -> loopM act x
+        Right v -> return v
+    
 sendDatagram :: S.Socket -> LByteString -> IO ()
 sendDatagram sock msg =
     S.sendAll sock $ encodeLazy (toEnum $ length msg :: Word32) <> msg
@@ -12,11 +20,16 @@ sendDatagram sock msg =
 receiveDatagram :: S.Socket -> IO LByteString
 receiveDatagram sock = do
     datagramLength <- S.recv sock 4
-    readMsg sock $ decodeLazy datagramLength 
+    rawMsg <- readMsg sock $ decodeLazy datagramLength
+    pure $ mconcat $ reverse rawMsg
 
-readMsg :: S.Socket -> Either String Word32 -> IO LByteString 
+readMsg :: S.Socket -> Either String Word32 -> IO [LByteString]
 readMsg _    (Left err)  = error $ "Decoding error " <> toText err
-readMsg _    (Right 0)   = pure ""
-readMsg sock (Right len) = do
-    msg <- S.recv sock ((toEnum.fromEnum) len)
-    (<>) <*> pure msg <$> readMsg sock (Right (len - toEnum (length msg)))
+readMsg sock (Right len) =
+    loopM (\(elemsOfMsg, restOfMsg) -> do
+        msg <- S.recv sock ((toEnum.fromEnum) restOfMsg)
+        let newLen = len - toEnum (length msg)
+        pure $ if newLen == 0
+            then Right $ msg : elemsOfMsg
+            else Left (msg : elemsOfMsg, newLen)
+    ) ([], len)
